707378637c55060adca93076914a80f725603d79
Mustafa Uslu <paqronnetwork@gmail.com>
2025-10-03 07:43:05 +0000

    Operatör kullanıcı profil bilgilerini kaldıralım

--- src/components/user-nav.tsx
+++ src/components/user-nav.tsx
@@ -5,10 +5,7 @@ import { Button } from "@/components/ui/button";
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { PlaceHolderImages } from "@/lib/placeholder-images";
@@ -30,20 +27,6 @@ export function UserNav() {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Operatör</p>
-            <p className="text-xs leading-none text-muted-foreground">
-              operator@conveyorguard.com
-            </p>
-          </div>
-        </DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuGroup>
-          <DropdownMenuItem>Profil</DropdownMenuItem>
-          <DropdownMenuItem>Ayarlar</DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
         <DropdownMenuItem>Çıkış Yap</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
