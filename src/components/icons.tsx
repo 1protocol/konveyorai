56dea21f20c3f86c801abb98500ba6e26e0c57fd
Mustafa Uslu <paqronnetwork@gmail.com>
2025-10-04 04:26:51 +0000

    otoyol çizgisi ikon (_for element path_)

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -17,12 +17,9 @@ export const Icons = {
         className={cn('text-primary', className)}
         {...props}
       >
-        {/* Triangle Ruler (Set Square) Icon */}
-        <path d="M21.7 3.3a1 1 0 0 0-1.4 0L3.3 20.3a1 1 0 0 0 0 1.4l.4.4a1 1 0 0 0 1.4 0L21.7 4.7a1 1 0 0 0 0-1.4l-.4-.4Z" />
-        <path d="M16 9h-2" />
-        <path d="M13 12H9" />
-        <path d="M10 15H8" />
-        <path d="M7 18H5" />
+        {/* Highway Lanes Icon */}
+        <path d="M7 22V2" />
+        <path d="M17 22V2" />
       </svg>
     );
   },
